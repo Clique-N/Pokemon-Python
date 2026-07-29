1d3d9c6ee3fe7cf76d46c918a947f56999f79f33
Guilherme <guisimoes2010@gmail.com>
2026-07-29 18:16:53 -0300

    Migrate database from sqlite to postgres

--- src/data.py
+++ src/data.py
@@ -8,8 +8,13 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
-    raise ValueError("DATABASE_URL not found in .env.")
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+    raise ValueError("DATABASE_URL not found.")
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
+    
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
